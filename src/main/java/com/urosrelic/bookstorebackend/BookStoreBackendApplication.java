ae64e5d2767637d78dff225f1f8e0bd66b90ca06
urosrelic <urosrelicc@gmail.com>
2024-03-08 19:21:32 +0100

    first commit

---
+++ src/main/java/com/urosrelic/bookstorebackend/BookStoreBackendApplication.java
@@ -0,0 +1,15 @@
+package com.urosrelic.bookstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class BookStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreBackendApplication.class, args);
+
+    }
+
+}
