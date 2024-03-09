ffb8ecf015caad9fd28bc51f521bc5dfd2fd5f35
urosrelic <urosrelicc@gmail.com>
2024-03-09 02:30:33 +0100

    feat: add spring security config

---
+++ src/main/java/com/urosrelic/bookstorebackend/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.urosrelic.bookstorebackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private static final String[] WHITELIST_URLS = {
+            "/users"
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(WHITELIST_URLS).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
