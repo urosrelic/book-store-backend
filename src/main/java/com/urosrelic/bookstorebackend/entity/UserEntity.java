a00842ea916d88d328176cf593d6907c821980d4
urosrelic <urosrelicc@gmail.com>
2024-03-10 14:58:49 +0100

    feat: update user entity username and emails columns to be unique

--- src/main/java/com/urosrelic/bookstorebackend/entity/UserEntity.java
+++ src/main/java/com/urosrelic/bookstorebackend/entity/UserEntity.java
@@ -11,11 +11,12 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique=true)
     private String username;
+    @Column(unique=true)
     private String email;
 //    @Size(min = 8, max = 16, message = "Password must be between 8 and 16 characters long")
     @Column(length = 60)
     private String password;
     private String role;
-    private boolean enabled = false;
 }
