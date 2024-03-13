28170c9bdb40d3976dfee9e48470d819dcd2fb17
urosrelic <urosrelicc@gmail.com>
2024-03-13 21:05:49 +0100

    feat: create AppConfig
    AppConfig contains RestTemplate and Gson beans

---
+++ src/main/java/com/urosrelic/bookstorebackend/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.urosrelic.bookstorebackend.config;
+
+import com.google.gson.Gson;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Gson gson() {
+        return new Gson();
+    }
+}
