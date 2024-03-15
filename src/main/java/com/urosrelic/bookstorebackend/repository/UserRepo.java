c5482264484dd79f669f7f498a5414fe4ea32a6e
urosrelic <urosrelicc@gmail.com>
2024-03-15 01:44:25 +0100

    feat: add findByEmail method

--- src/main/java/com/urosrelic/bookstorebackend/repository/UserRepo.java
+++ src/main/java/com/urosrelic/bookstorebackend/repository/UserRepo.java
@@ -4,6 +4,6 @@ import com.urosrelic.bookstorebackend.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<UserEntity, Integer> {
-
+    UserEntity findByEmail(String email);
     UserEntity findByUsername(String username);
 }
