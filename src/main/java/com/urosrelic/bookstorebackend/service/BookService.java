596032e7b94d18ffedaef0b08707b43163cfbeda
urosrelic <urosrelicc@gmail.com>
2024-03-17 02:28:30 +0100

    chore: optimize imports

--- src/main/java/com/urosrelic/bookstorebackend/service/BookService.java
+++ src/main/java/com/urosrelic/bookstorebackend/service/BookService.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
