185ae07cfb06082270b0fa69fbdb2ba02ac2ff25
urosrelic <urosrelicc@gmail.com>
2024-03-22 17:08:20 +0100

    feat: add repos for purchase and purchase item

--- src/main/java/com/urosrelic/bookstorebackend/repository/PurchaseRepo.java
+++ src/main/java/com/urosrelic/bookstorebackend/repository/PurchaseRepo.java
@@ -1,9 +1,13 @@
 package com.urosrelic.bookstorebackend.repository;
 
 import com.urosrelic.bookstorebackend.entity.Purchase;
+import com.urosrelic.bookstorebackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PurchaseRepo extends JpaRepository<Purchase, Long> {
+    List<Purchase> findByUser(User user);
 }
